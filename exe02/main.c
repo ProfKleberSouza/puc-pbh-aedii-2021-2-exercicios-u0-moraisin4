d1a03436f27fadb4733affe2b134743dabceb6b5
Gabriel de Souza Morais <63837330+moraisin4@users.noreply.github.com>
2021-08-09 21:04:33 +0000

    Exercício 2

--- exe02/main.c
+++ exe02/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
+   int n1, n2, n3;
+   float dist;
+   scanf("%i %i %i", &n1, &n2, &n3);
+   printf("R = %i\n", n3);
+   printf("V = %i\n", n2);
+   printf("T = %i\n", n1);
+   printf("D = %i\n", n1*n2);
+   printf("L = %d\n", (n1*n2)/n3);
    return 0;
 }
